b343e31b6a16f90bfa5adf84ab4d8e5750546c17
KimYongchan_home <yczine@gmail.com>
2023-06-03 04:24:46 +0800

    test feature(full text search with algoria

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:algolia/algolia.dart';
 import 'package:logger/logger.dart';
 import 'package:today_safety/service/provider/provider_user.dart';
 
@@ -5,4 +6,8 @@ class MyApp {
   static Logger logger = Logger();
 
   static ProviderUser providerUser = ProviderUser();
+  static const Algolia algolia = Algolia.init(
+    applicationId: 'MTRJRT9R1H',
+    apiKey: 'dac0a057b6d123949b76313b80fa6d48',
+  );
 }
--- lib/ui/route/route_main.dart
+++ lib/ui/route/route_main.dart
@@ -1,9 +1,10 @@
+import 'package:algolia/algolia.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:today_safety/const/value/router.dart';
 
 import '../../const/value/key.dart';
-
+import '../../my_app.dart';
 
 class RouteMain extends StatelessWidget {
   const RouteMain({Key? key}) : super(key: key);
@@ -22,12 +23,22 @@ class RouteMain extends StatelessWidget {
               },
               child: Text('인증 페이지로'),
             ),
-
             ElevatedButton(
               onPressed: () {
                 Get.toNamed(keyRouteLogin);
               },
               child: Text('로그인 페이지로'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                AlgoliaQuery algoliaQuery = MyApp.algolia.instance.index('fts_site').query("현대 건설");
+                AlgoliaQuerySnapshot snap = await algoliaQuery.getObjects();
+                MyApp.logger.d("알고리아 결과 hit 갯수 : ${snap.hits.length}");
+                snap.hits.forEach((element) {
+                  print("문서 id : ${element.objectID}");
+                });
+              },
+              child: Text('풀텍스트 검색 테스트'),
             ),
           ],
         ),
