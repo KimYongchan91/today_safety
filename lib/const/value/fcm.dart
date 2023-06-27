c267f603f733b316c84c8fb0b2cf5ab32b90c3b9
KimYongchan_home <yczine@gmail.com>
2023-06-27 17:41:56 +0800

    add feature - send fcm new check to master2

--- lib/const/value/fcm.dart
+++ lib/const/value/fcm.dart
@@ -2,21 +2,21 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 const AndroidNotificationChannel channelNoticeNew = AndroidNotificationChannel(
   'NEW_NOTICE', // id
-  '새 공지사항 알림', // title
+  '새 공지사항', // title
   description: '근무지에서 게시한 새 공지사항을 알려줘요.', // description
   importance: Importance.max,
 );
 
 const AndroidNotificationChannel channelCheckNew = AndroidNotificationChannel(
   'NEW_CHECK', // id
-  '새 인증 확인 요청 알림', // title
+  '새 인증 확인 요청', // title
   description: '팀원이 제출한 새 인증 확인 요청을 알려줘요.', // description
   importance: Importance.max,
 );
 
 const AndroidNotificationChannel channelCheckResult = AndroidNotificationChannel(
   'CHECK_RESULT', // id
-  '인증 확인 결과 알림', // title
+  '인증 확인 결과', // title
   description: '제출한 인증에 대한 확인 결과를 알려줘요.', // description
   importance: Importance.max,
 );
