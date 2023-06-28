04b19da1a72cde7aa58dbca7cc05aca394f304d0
yd <dyuo0619@gmail.com>
2023-06-28 21:37:33 +0900

    color

--- lib/const/value/color.dart
+++ lib/const/value/color.dart
@@ -5,7 +5,7 @@ const Color colorGreyTextSub = Colors.grey;
 const Color colorBackground = Color(0xfff3f3f3);
 const Color colorAppPrimary = Color(0xffffb800);
 
-const Color colorCheckStateOn = Colors.green;
-const Color colorCheckStatePend = colorAppPrimary;
-const Color colorCheckStateReject = Colors.red;
+const Color colorCheckStateOn = Colors.greenAccent;
+const Color colorCheckStatePend = Colors.orangeAccent;
+const Color colorCheckStateReject = Colors.redAccent;
 
