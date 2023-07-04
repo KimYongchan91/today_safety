50a634ddcdc476fb55d1b1dd92a001432da0d063
yczine@gmail.com <yczine@gmail.com>
2023-07-04 21:13:09 +0900

    re design - qr scan

---
+++ lib/const/value/admin.dart
@@ -0,0 +1,5 @@
+const List<String> listAdmin = [
+  'yczine@naver.com&lt=kakao',
+  'yczinetest@kakao.com&lt=kakao',
+  'tmc91@naver.com&lt=naver',
+];
--- lib/ui/widget/widget_app_bar.dart
+++ lib/ui/widget/widget_app_bar.dart
@@ -3,17 +3,20 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../const/value/admin.dart';
+import '../../my_app.dart';
 import '../route/test/route_test.dart';
 
 class WidgetAppBar extends StatelessWidget {
   final Color colorBackground;
   final double paddingHorizontal;
+
   const WidgetAppBar({this.colorBackground = Colors.white, this.paddingHorizontal = 10, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding:  EdgeInsets.symmetric(horizontal: paddingHorizontal),
+      padding: EdgeInsets.symmetric(horizontal: paddingHorizontal),
       width: MediaQuery.of(context).size.width,
       height: 65,
       color: colorBackground,
@@ -22,7 +25,9 @@ class WidgetAppBar extends StatelessWidget {
           ///아이콘
           InkWell(
             onTap: () {
-              Get.to(() => const RouteTest());
+              if (MyApp.providerUser.modelUser != null && listAdmin.contains(MyApp.providerUser.modelUser!.id)) {
+                Get.to(() => const RouteTest());
+              }
             },
             child: ClipRRect(
               borderRadius: BorderRadius.circular(4),
@@ -34,16 +39,20 @@ class WidgetAppBar extends StatelessWidget {
                   color: Colors.yellow.shade700,
                   borderRadius: BorderRadius.circular(5),
                 ),
-                child:Image.asset('assets/images/logo/app_logo_big_128.png',width: 40,height: 40,),
+                child: Image.asset(
+                  'assets/images/logo/app_logo_big_128.png',
+                  width: 40,
+                  height: 40,
+                ),
               ),
             ),
           ),
           const SizedBox(
             width: 10,
           ),
-           const Text(
+          const Text(
             '오늘안전',
-           style: TextStyle(fontFamily: "SANGJU",fontSize: 23),
+            style: TextStyle(fontFamily: "SANGJU", fontSize: 23),
           ),
         ],
       ),
